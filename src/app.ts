59080e6fca101363561db9b35cee0471e77973bd
Ohm Piromrak <>
2022-02-28 18:10:24 +0700

    add app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,23 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import compression from 'compression';
 
-const app = express();
+export const app = express();
+
+app.use(helmet());
+app.use(cors());
+
+app.use(compression());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// routes
 
 export default app;
